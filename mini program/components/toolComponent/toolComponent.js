c0b3871d5eecee441aa76d719de440329a55fe49
moqi <1402290478@qq.com>
2021-03-25 21:05:40 +0800

    工具部分搭建完毕

---
+++ mini program/components/toolComponent/toolComponent.js
@@ -0,0 +1,34 @@
+// components/toolComponent/toolComponent.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+      imageUrl:{
+        type:String,
+        value:"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1391354859,1949870536&fm=26&gp=0.jpg"
+      },
+      toolName:{
+        type:String,
+        value:"工具名"
+      },
+      intruction:{
+        type:String,
+        value:"简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介"
+      }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
