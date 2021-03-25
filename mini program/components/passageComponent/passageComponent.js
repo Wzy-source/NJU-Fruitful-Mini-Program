bd12211b843aa28f66b5d89db2ef2a8f9e781f73
moqi <1402290478@qq.com>
2021-03-25 20:35:37 +0800

    完成了首页前端界面的搭建

---
+++ mini program/components/passageComponent/passageComponent.js
@@ -0,0 +1,34 @@
+// components/passageComponent/passageComponent.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+      imageUrl:{
+        type:String,
+        value:"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1391354859,1949870536&fm=26&gp=0.jpg"
+      },
+      title:{
+        type:String,
+        value:"文章标题"
+      },
+      passage:{
+        type:String,
+        value:"文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容"
+      }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
