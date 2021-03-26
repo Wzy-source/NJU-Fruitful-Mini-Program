e15d135039768999bb4675cf89ff5b377437b070
茫茫燃 <191830154@smail.nju.edu.cn>
2021-03-26 20:24:12 +0800

    修改了主页的样式，并将主页的资讯部分组件化

---
+++ mini program/components/newsComponent/newsComponent.js
@@ -0,0 +1,36 @@
+// components/newsComponent/newsComponent.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    myNewsTime:{
+      type:String,
+      value:""
+    },
+    myNewsTitle:{
+      type:String,
+      value:""
+    },
+    myNewsImgUrl:{
+      type:String,
+      value:""
+    }
+
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
