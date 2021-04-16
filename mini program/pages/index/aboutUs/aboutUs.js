a15e63a369a284467148825b328ed04471843197
茫茫燃 <191830154@smail.nju.edu.cn>
2021-04-16 22:29:13 +0800

    更改了marker图标

--- mini program/pages/index/aboutUs/aboutUs.js
+++ mini program/pages/index/aboutUs/aboutUs.js
@@ -12,7 +12,11 @@ Page({
       // 甘肃兰州
       longitude: 103.840521,
       latitude: 36.067235,
-      name:'刘冠宇'
+      name:'刘冠宇',
+      iconPath:"../../../images/map-marker.png",
+      height:30,
+      width:30
+      
         
       },
       {
@@ -20,7 +24,10 @@ Page({
         // 西安
         longitude: 108.9608,
         latitude: 34.26641,
-        name:'王子悦'
+        name:'王子悦',
+        iconPath:"../../../images/map-marker.png",
+        height:30,
+        width:30
         
       },
       {
@@ -28,21 +35,30 @@ Page({
         // 安徽合肥
         longitude: 117.233443,
         latitude: 31.826578,
-        name:"郝月婷"
+        name:"郝月婷",
+        iconPath:"../../../images/map-marker.png",
+        height:30,
+        width:30
       },
       {
         id:4,
         // 河北石家庄
         longitude: 114.521532,
         latitude: 38.048312,
-        name:'刘琪'
+        name:'刘琪',
+        iconPath:"../../../images/map-marker.png",
+        height:30,
+        width:30
       },
       {
         id:5,
         // 山东济南
         longitude: 117.126399,
         latitude: 36.656554,
-        name:'王可心'
+        name:'王可心',
+        iconPath:"../../../images/map-marker.png",
+        height:30,
+        width:30
 
       },
       {
@@ -50,15 +66,20 @@ Page({
         // 福建福州
         longitude: 119.30347,
         latitude: 26.080429,
-        name:'何馨'
-
+        name:'何馨',
+        iconPath:"../../../images/map-marker.png",
+        height:30,
+        width:30
       },
       {
         id:7,
         // 贵州贵阳
         longitude: 107.886884,
         latitude: 25.415372,
-        name:'姜兴黔'
+        name:'姜兴黔',
+        iconPath:"../../../images/map-marker.png",
+        height:30,
+        width:30
       },
       {
         id: 8,
@@ -66,13 +87,19 @@ Page({
         longitude: 119.327869,
         latitude: 34.092698,
         name: '周银邱',
+        iconPath:"../../../images/map-marker.png",
+        height:30,
+        width:30
       },
       {
         id:9,
         // 新疆乌鲁木齐
         longitude: 87.635087,
         latitude: 43.79934,
-        name:'杨艺澜'
+        name:'杨艺澜',
+        iconPath:"../../../images/map-marker.png",
+        height:30,
+        width:30
         
       },
      
@@ -81,7 +108,10 @@ Page({
         // 广东广州
         longitude: 113.271431,
         latitude: 23.135336,
-        name:'左佳毅'
+        name:'左佳毅',
+        iconPath:"../../../images/map-marker.png",
+        height:30,
+        width:30
         
       },
 
@@ -90,28 +120,40 @@ Page({
         // 福建宁德
         longitude: 119.52933,
         latitude: 26.656741,
-        name:'郭哲强'
+        name:'郭哲强',
+        iconPath:"../../../images/map-marker.png",
+        height:30,
+        width:30
       },
       {
         id:12,
         // 新疆乌鲁木齐
         longitude: 84.416029,
         latitude: 43.477086,
-        name:'摆正航'
+        name:'摆正航',
+        iconPath:"../../../images/map-marker.png",
+        height:30,
+        width:30
       },
       {
         id:13,
         // 江苏南通
         longitude: 120.794181,
         latitude: 32.42594,
-        name:'任冒语'
+        name:'任冒语',
+        iconPath:"../../../images/map-marker.png",
+        height:30,
+      width:30
       },
       {
         id:14,
         // 安徽滁州
         longitude: 118.339406,
         latitude: 32.261271,
-        name:'张刘洋'
+        name:'张刘洋',
+        iconPath:"../../../images/map-marker.png",
+        height:30,
+        width:30
         
       },
       {
@@ -119,7 +161,10 @@ Page({
         // 江苏苏州
         longitude: 120.592412,
         latitude: 31.303564,
-        name:'姜玲玲'
+        name:'姜玲玲',
+        iconPath:"../../../images/map-marker.png",
+        height:30,
+      width:30
         
 
       },
@@ -128,7 +173,10 @@ Page({
         // 福建莆田
         longitude: 119.141775,
         latitude: 25.086626,
-        name:'沈正钦'
+        name:'沈正钦',
+        iconPath:"../../../images/map-marker.png",
+      height:30,
+      width:30
       }
 
 
